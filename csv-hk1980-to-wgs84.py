d4fad2765fad99560c89135f8656ce69032b6138
Abel <deaddog@rach.(none)>
2009-06-07 00:20:13 +0800

    Quote fields when writing CSV

--- csv-hk1980-to-wgs84.py
+++ csv-hk1980-to-wgs84.py
@@ -47,10 +47,10 @@ class UnicodeWriter:
 	which is encoded in the given encoding.
 	"""
 
-	def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+	def __init__(self, f, dialect=csv.excel, quoting=csv.QUOTE_MINIMAL, encoding="utf-8", **kwds):
 		# Redirect output to a queue
 		self.queue = cStringIO.StringIO()
-		self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+		self.writer = csv.writer(self.queue, dialect=dialect, quoting=quoting, **kwds)
 		self.stream = f
 		self.encoder = codecs.getincrementalencoder(encoding)()
 
